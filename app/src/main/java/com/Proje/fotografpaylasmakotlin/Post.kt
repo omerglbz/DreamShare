2c5b7a7efc37074ea81793adda40d873125f7e79
Omer Faruk Gulbuz <gulbuzomer@gmail.com>
2023-01-15 09:50:40 +0300

    androidproject

---
+++ app/src/main/java/com/Proje/fotografpaylasmakotlin/Post.kt
@@ -0,0 +1,13 @@
+package com.Proje.fotografpaylasmakotlin
+
+import android.widget.Button
+
+class Post(
+    var kullaniciEmail: String,
+    var kullaniciYorum: String,
+    var gorselUrl: String
+) {
+
+
+
+}
